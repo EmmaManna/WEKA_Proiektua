0d3f1c2e6bf2adbf319a1491cfb1c6379c67b5f4
emmam <emmamanna00@gmail.com>
2021-03-27 11:23:11 +0100

    Train multzoarekin errepresentazio bektoriala eta ez dispertsoa sortu

--- src/main/java/ehu/weka/Main.java
+++ src/main/java/ehu/weka/Main.java
@@ -1,7 +1,37 @@
 package ehu.weka;
 
+import weka.core.Instances;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+        //Datuak Kargatu
+        if(args.length!=3){
+            System.out.println("Atazak:");
+            System.out.println("\t 1. Datu sortako string atributuak bektorizatu ");
+
+            System.out.println("\nArgumentuak:");
+            System.out.println("\t 1. Datu sortaren kokapena (path) .arff  formatuan (input).");
+            System.out.println("\t 2. Bektorizatutako atributuak dituzten datuak gordetzeko path");
+            System.out.println("\t 3. Errepresentazio dispertsoa dituzten datuak gordetzeko path");
+            System.exit(0);
+        }
+
+        Proiektua p = Proiektua.getInstance();
+
+        //Datuak kargatu
+        Instances data = p.datuakKargatu(args[0]);
+
+        //StringToWordVector
+        Instances vectorData = p.stringToVector(data);
+
+        //Atributu hoberenak lortu
+        vectorData = p.selection(vectorData);
+        p.datuakGorde(args[1],vectorData);
+
+        //NonSparse
+        Instances nonSparseData = p.nonSparse(vectorData);
+        p.datuakGorde(args[2],nonSparseData);
+
 
     }
 }
--- src/main/java/ehu/weka/Proiektua.java
+++ src/main/java/ehu/weka/Proiektua.java
@@ -1,7 +1,15 @@
 package ehu.weka;
 
 import weka.core.Instances;
+import weka.core.converters.ArffSaver;
 import weka.core.converters.ConverterUtils;
+import weka.filters.Filter;
+import weka.filters.supervised.attribute.AttributeSelection;
+import weka.filters.unsupervised.attribute.StringToWordVector;
+import weka.filters.unsupervised.instance.SparseToNonSparse;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Proiektua {
     private static final Proiektua instance = new Proiektua();
@@ -19,5 +27,34 @@ public class Proiektua {
         Instances data = source.getDataSet();
         data.setClassIndex(data.numAttributes()-1);
         return data;
+    }
+
+    public void datuakGorde(String path, Instances data) throws IOException {
+        ArffSaver saver = new ArffSaver();
+        saver.setInstances(data);
+        saver.setFile(new File(path));
+        saver.writeBatch();
+    }
+
+    public Instances stringToVector(Instances data) throws Exception {
+        StringToWordVector filterVector = new StringToWordVector();
+        filterVector.setInputFormat(data);
+        filterVector.setWordsToKeep(Integer.MAX_VALUE);
+        Instances vectorData = Filter.useFilter(data,filterVector);
+        return vectorData;
+    }
+
+    public Instances selection(Instances data) throws Exception {
+        AttributeSelection filterSelection = new AttributeSelection();
+        filterSelection.setInputFormat(data);
+        Instances selection = Filter.useFilter(data,filterSelection);
+        return selection;
+    }
+
+    public Instances nonSparse(Instances data)throws Exception {
+        SparseToNonSparse filterNonSparse = new SparseToNonSparse();
+        filterNonSparse.setInputFormat(data);
+        Instances nonSparseData = Filter.useFilter(data,filterNonSparse);
+        return nonSparseData;
     }
 }
