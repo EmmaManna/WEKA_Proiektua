2c736bc32f9c90fd63d7af6308229f4ddf5def68
emmam <emmamanna00@gmail.com>
2021-04-07 23:20:26 +0200

    Aurreprozesamendua interfaze grafikoan eginda

--- src/main/java/ehu/weka/uiControllers/PreprocessKudeatzaile.java
+++ src/main/java/ehu/weka/uiControllers/PreprocessKudeatzaile.java
@@ -1,5 +1,8 @@
 package ehu.weka.uiControllers;
 
+import ehu.weka.Atal1.GetRaw;
+import ehu.weka.Atal1.MakeCompatible;
+import ehu.weka.Atal1.TransformRaw;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -47,8 +50,38 @@ public class PreprocessKudeatzaile implements Initializable {
     private TextArea txtA_egoera;
 
     @FXML
-    void onClick(ActionEvent event) {
+    void onClick(ActionEvent event) throws Exception {
         if(event.getSource()==btn_start){
+            if(trainFile!=null && testFile!=null){
+                String path = trainFile.substring(0,trainFile.length()-lbl_train.getText().length());
+                String[] argumentsTrain = new String[] {"train",trainFile,path+lbl_train.getText().substring(0,lbl_train.getText().length()-4)+".arff"};
+                GetRaw.main(argumentsTrain);
+                txtA_egoera.setText("Train CSV fitxategia egokituta");
+                String[] argumentsTest = new String[] {"test",testFile,path+lbl_test.getText().substring(0,lbl_test.getText().length()-4)+".arff"};
+                GetRaw.main(argumentsTest);
+                txtA_egoera.appendText("\nTest CSV fitxategia egokituta");
+
+                String bow = "1";
+                if(rdbtn_BoW.isSelected()){
+                    bow = "0";
+                }
+
+                String sparse = "no";
+                if(rdbtn_sparse.isSelected()){
+                    sparse = "yes";
+                }
+
+                String[] argumentsTR = new String[] {path+lbl_train.getText().substring(0,lbl_train.getText().length()-4)+".arff",bow,sparse,path+"train"+bow+sparse+".arff"};
+                TransformRaw.main(argumentsTR);
+                txtA_egoera.appendText("\nTrain fitxategia transformatuta: BoW="+bow+" , Sparse="+sparse+" , path="+path+"train"+bow+sparse+".arff");
+
+                String[] argumentsMC = new String[] {path+"train"+bow+sparse+".arff",path+lbl_test.getText().substring(0,lbl_test.getText().length()-4)+".arff",bow,sparse,path+"test"+bow+sparse+".arff"};
+                MakeCompatible.main(argumentsMC);
+                txtA_egoera.appendText("\nTest fitxategia transformatuta: BoW="+bow+" , Sparse="+sparse+" , path="+path+"test"+bow+sparse+".arff");
+            }
+            else{
+                txtA_egoera.setText("Test eta train csv fitxategiak kargatu mesedez");
+            }
 
         } else if(event.getSource()==btn_train){
             FileChooser fileChooser = new FileChooser();
